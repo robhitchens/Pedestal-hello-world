91e8d3f0f46b777e40ef0aceceb7effd6a85cb69
Robert Hitchens <rjhitchens3@gmail.com>
2020-08-29 15:43:11 -0400

    - added functions to run server in development mode so the same repl can continued to be used.

--- src/hello.clj
+++ src/hello.clj
@@ -3,17 +3,31 @@
             [io.pedestal.http.route :as route]))
 
 (defn respond-hello [request]
-  {:status 200 :body "Testing out reload function!"})
+  {:status 200 :body "Hello World!"})
 
 (def routes
   (route/expand-routes
    #{[ "/greet" :get respond-hello :route-name :greet]}))
 
-(defn create-server []
-  (http/create-server
-   {::http/routes routes
-    ::http/type   :jetty
-    ::http/port   8890}))
+(def service-map
+  {::http/routes routes
+   ::http/type   :jetty
+   ::http/port   8890})
 
 (defn start []
-  (http/start (create-server)))
+  (http/start (http/create-server service-map)))
+
+(defonce server (atom nil))
+
+(defn start-dev []
+  (reset! server 
+          (http/start (http/create-server
+                       (assoc service-map
+                              ::http/join? false)))))
+
+(defn stop-dev []
+  (http/stop @server))
+
+(defn restart []
+  (stop-dev)
+  (start-dev))
