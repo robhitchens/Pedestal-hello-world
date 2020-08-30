7a46ab1d6ceeb2ff76bd7ac3b2150a4a6e0c8b68
Robert Hitchens <rjhitchens3@gmail.com>
2020-08-30 14:46:18 -0400

    - refactored coerce-body function out into several functions to make code more readable

--- src/hello.clj
+++ src/hello.clj
@@ -6,10 +6,6 @@
 
 (def unmentionalbles #{"YHWH" "Voldemort" "Mxyzptlk" "Rumplestiltskin" "曹操"})
 
-(def supported-types ["text/html" "application/edn" "application/json" "text/plain"])
-
-(def content-neg-intc (conneg/negotiate-content supported-types))
-
 (defn ok [body]
   {:status 200 :body body})
 
@@ -31,27 +27,37 @@
 
 (def echo
   {:name ::echo
-   :enter (fn [context]
-            (let [request (:request context)
-                  response (ok request)]
-              (assoc context :response response)))})
+   :enter #(assoc % :response (ok (:request %)))})
+
+(def supported-types ["text/html" "application/edn" "application/json" "text/plain"])
+
+(def content-neg-intc (conneg/negotiate-content supported-types))
+
+(defn accepted-type
+  [context]
+  (get-in context [:request :accept :field] "text/plain"))
+
+(defn transform-content
+  [body content-type]
+  (case content-type
+    "text/html"         body
+    "text/plain"        body
+    "application/edn"   (pr-str body)
+    "application/json"  (json/write-str body)))
+
+(defn coerce-to
+  [response content-type]
+  (-> response
+      (update :body transform-content content-type)
+      (assoc-in [:headers "Content-Type"] content-type)))
 
 (def coerce-body
   {:name ::coerce-body
    :leave 
    (fn [context]
-     (let [accepted      (get-in context [:request :accept :field] "text/plain")
-           response      (get context :response)
-           body          (get response :body)
-           coerced-body  (case accepted
-                           "text/html"        body
-                           "text/plain"       body
-                           "application/edn"  (pr-str body)
-                           "application/json" (json/write-str body))
-           updated-response (assoc response
-                                   :headers {"Content-Type" accepted}
-                                   :body    coerced-body)]
-       (assoc context :response updated-response)))})
+     (cond-> context
+       (nil? (get-in context [:response :headers "Content-Type"]))
+       (update-in [:response] coerce-to (accepted-type context))))})
  
 (def routes
   (route/expand-routes
